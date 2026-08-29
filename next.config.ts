fce32bb26e43eca68f0001d8977059d93ea49176
pacharapol pimpa <thefirestong@gmail.com>
2026-08-29 19:35:48 +0700

    chore: update docker

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+<<<<<<< HEAD
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "**" },
     ],
   },
+=======
+  /* config options here */
+  reactCompiler: true,
+  output: "standalone",
+>>>>>>> 7f14b31 (chore: initial docker)
 };
 
 export default nextConfig;
